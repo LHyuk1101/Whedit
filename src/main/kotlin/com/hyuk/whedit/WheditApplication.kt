0a72dc5bfc1617621be2ccfc132844ab5dd38659
LHyuk1101 <lhdev1101@gmail.com>
2024-10-02 00:58:55 +0900

    first commit

---
+++ src/main/kotlin/com/hyuk/whedit/WheditApplication.kt
@@ -0,0 +1,11 @@
+package com.hyuk.whedit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WheditApplication
+
+fun main(args: Array<String>) {
+  runApplication<WheditApplication>(*args)
+}
---
+++ src/test/kotlin/com/hyuk/whedit/WheditApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hyuk.whedit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WheditApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
